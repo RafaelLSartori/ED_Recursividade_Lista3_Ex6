5ecae83dc7dac1f4eab7363d3b607436ddf5857c
Rafael Sartori <160548884+RafaelLSartori@users.noreply.github.com>
2024-08-16 12:19:13 -0300

    Atualização

--- src/controller/Ex6.java
+++ src/controller/Ex6.java
@@ -1,2 +1,25 @@
-package controller;public class Ex6 {
+package controller;
+
+public class Ex6 {
+    public Ex6(){
+        super();
+    }
+    public double frecserie(int n){
+        //Ponto de parada: Se n for igual a 1, seria 1/1 que resulta em 1.
+        if( n == 1 ){
+            return 1;
+        }else{
+            //se não for igual a 1, eu chamo a função recursiva do fatorial no denominador + a função recursiva da série diminindo n.
+            return (1/frecfat(n)) + frecserie(n - 1);
+        }
+    }
+    public double frecfat(int n){
+        //Ponto de parada: Fatorial de 0 e 1 é igual a 1.
+        if( n == 0 || n == 1){
+            return 1;
+        }else{
+            //Aqui chamo a recursiva do fatorial multiplicando n pelo seu antecessor (n-1) até chegar a 1.
+            return n * frecfat(n-1 );
+        }
+    }
 }
--- src/view/Main.java
+++ src/view/Main.java
@@ -1,5 +1,14 @@
+package view;
+
+import controller.Ex6;
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Ex6 m = new Ex6();
+        int num = Integer.parseInt(JOptionPane.showInputDialog("Digite quantas vezes a série irá se repetir: "));
+        double resp = m.frecserie(num);
+        System.out.println(resp);
     }
 }
